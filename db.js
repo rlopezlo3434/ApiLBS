3aa327fda2743cadc8a094d7267790aba0530757
Renzo <152234907+rlopezlo3434@users.noreply.github.com>
2025-08-09 23:19:37 -0500

    .

--- db.js
+++ db.js
@@ -1,19 +1,22 @@
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: 'centerbeam.proxy.rlwy.net',
   user: "root",
   port: 40313,
   password: "rOiGqhPtRFabuJmnBFHdhoDlHQcgtUug",
   database: "railway",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-connection.connect((err) => {
-  if (err) {
-    console.error("Error conectando a MySQL:", err);
-    return;
-  }
-  console.log("Conectado a MySQL");
-});
+// connection.connect((err) => {
+//   if (err) {
+//     console.error("Error conectando a MySQL:", err);
+//     return;
+//   }
+//   console.log("Conectado a MySQL");
+// });
 
 module.exports = connection;
